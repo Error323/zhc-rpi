d3e7efeb8b0334aea98a7c74fba43ce1d3e84594
Error323 <folkerthuizinga@gmail.com>
2016-12-22 20:35:11 +0100

    show current day cost

--- html/cost.js
+++ html/cost.js
@@ -1,16 +1,13 @@
 $(function() {
   // constants - computed from energy bills
-  var HIGH_EURO = 0.19862318840579712;
-  var LOW_EURO = 0.1833774834437086;
-  var GAS_EURO = 0.689871794871795;
-  var BASE_URL = 'http://utrecht.kubiko.nl:8082/nodes';
+  const HIGH_EURO = 0.19862318840579712;
+  const LOW_EURO = 0.1833774834437086;
+  const GAS_EURO = 0.689871794871795;
+  const BASE_URL = 'http://utrecht.kubiko.nl:8082/nodes';
+  const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   var counter = 0;
-  var euro_high = [];
-  var euro_low = [];
-  var euro_gas = [];
   var raw_data = [];
-  var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   // plot options
   var options = {
@@ -29,12 +26,11 @@ $(function() {
     }
   };
 
-  function update_graph() {
-  }
 
   function euro_formatter(v, axis) {
     return '&euro;' + v;
   }
+
 
   function on_data_received(values) {
     counter++;
@@ -42,10 +38,13 @@ $(function() {
 
     if (counter == 8) {
       raw_data.sort();
+      var euro_high = [];
+      var euro_low = [];
+      var euro_gas = [];
 
       for (i = 0; i < 7; i++) {
         date = new Date(raw_data[i+1][0]);
-        options.xaxis.ticks.push([i, days[date.getDay()]]);
+        options.xaxis.ticks.push([i, DAYS[date.getDay()]]);
         euro_low.push([i, (raw_data[i+1][1][0] - raw_data[i][1][0])*LOW_EURO]);
         euro_high.push([i, (raw_data[i+1][1][1] - raw_data[i][1][1])*HIGH_EURO]);
         euro_gas.push([i, (raw_data[i+1][1][4] - raw_data[i][1][4])*GAS_EURO]);
@@ -60,15 +59,27 @@ $(function() {
     }
   }
 
+
   function load_graph() {
+    counter = 0;
+    raw_data = [];
+    options.xaxis.ticks = [];
     now = new Date();
     end = new Date(now.getTime() - (now.getHours()*3600 + now.getMinutes()*60 + now.getSeconds()+1)*1000);
-    start = new Date(end.getTime() - 3600*24*7*1000);
+    start = new Date(end.getTime() - 3600*24*6*1000);
     start = Math.floor(start / 1000);
+    var url = BASE_URL + '/8000736D65746572/values';
 
-    for (i = 0; i < 8; i++) {
+    $.ajax({
+      url: url,
+      method: 'GET',
+      dataType: 'json',
+      success: on_data_received
+    });
+
+    for (i = 0; i < 7; i++) {
       time = start + i*3600*24;
-      var url = BASE_URL + '/8000736D65746572/values/' + time;
+      url = BASE_URL + '/8000736D65746572/values/' + time;
 
       $.ajax({
         url: url,
@@ -81,6 +92,6 @@ $(function() {
 
   $(document).ready(function () {
     load_graph();
-    window.setInterval(update_graph, 10000);
+    window.setInterval(load_graph, 15000);
   });
 });
